2c8927501e02ddcde9e7e13cc628be94bbc905fd
CarloMicieli <carlo.micieli@gmail.com>
2023-02-20 08:06:08 +0100

    chore: add the gradle build

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.15.0")
+    implementation("io.spring.gradle:dependency-management-plugin:1.1.0")
+    implementation("me.qoomon:gradle-git-versioning-plugin:6.4.2")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.2")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.45.0")
+}
